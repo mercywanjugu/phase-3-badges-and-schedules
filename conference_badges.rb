ac6a0a76aae6668bc37cf85dd05b93f9a1faa5e2
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-08-20 12:11:13 +0300

    commit tree ee2279e0c4d149e1dac8558c902c7adf02fa2e23

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map{|name|badge_maker(name)}
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index do|speaker,index|
+        "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+    end
+end
+
+def printer (names)
+    batch_badge_creator(names).map{|badge|puts badge}
+    assign_rooms(names).map {|room|puts room}
+end
+printer(["Arel","Carol"])
